393a5bae0ebe0027337c9cac67332f66c9ed5bcd
YuliiaTsaruk <julia.tsaruck@gmail.com>
2023-11-12 14:02:35 +0100

    add notification component

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,3 +1,4 @@
+import { Notification } from '../Notification/Notification';
 export const Statistics = ({
   state: { good, neutral, bad },
   countPositiveFeedback,
@@ -5,11 +6,17 @@ export const Statistics = ({
 }) => {
   return (
     <div>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {total}</p>
-      <p>Positive feedback: {countPositiveFeedback}%</p>
+      {total > 0 ? (
+        <div>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {total}</p>
+          <p>Positive feedback: {countPositiveFeedback}%</p>
+        </div>
+      ) : (
+        <Notification message="There is no feedback"></Notification>
+      )}
     </div>
   );
 };
